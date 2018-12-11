0e30657d8537467ba628cf1e42fe7753af162141
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-11 14:49:57 +0100

    inizio gestione click sul quadrato

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
   //assegno variabili con le posizione dei tag
   var contenitore = $('.container');
   //genero i 36 quadrattini
-  for (var i = 0; i <= 36; i++) {
+  for (var i = 0; i < 36; i++) {
     console.log(i);
     var template = $('#griglia .quadrato').clone();
     contenitore.append(template);
   }
+  //gestisco il click sul quadrato
+  $('.quadrato').click(function(){
+    alert("esisto");
+  });
+
 
 });
