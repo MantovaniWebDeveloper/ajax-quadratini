40347dee15f98dd9fc400101291a72eb60e47df8
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-11 15:01:25 +0100

    creata la chiamata in lettura ajax e gestiti gli errori

--- js/main.js
+++ js/main.js
@@ -3,13 +3,24 @@ $(document).ready(function(){
   var contenitore = $('.container');
   //genero i 36 quadrattini
   for (var i = 0; i < 36; i++) {
-    console.log(i);
     var template = $('#griglia .quadrato').clone();
     contenitore.append(template);
   }
   //gestisco il click sul quadrato
   $('.quadrato').click(function(){
-    alert("esisto");
+    //alert("esisto");
+    //invoco la chiamata
+    $.ajax({
+      url: "https://www.boolean.careers/api/random/int",
+      method: "GET",
+      success: function(data,stato){
+        console.log(data.response);
+
+      },
+      error: function(richiesta,stato,errori){
+        console.log("c'è stato un errore " + errori);
+      }
+    });
   });
 
 
