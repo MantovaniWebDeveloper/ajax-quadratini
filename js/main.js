f5dee18b8bdb5edea4b4c1616602d707ad2e9e4c
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-11 14:47:31 +0100

    generati i quadratini via jquery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-//genero i 36 quadrattini
+$(document).ready(function(){
+  //assegno variabili con le posizione dei tag
+  var contenitore = $('.container');
+  //genero i 36 quadrattini
+  for (var i = 0; i <= 36; i++) {
+    console.log(i);
+    var template = $('#griglia .quadrato').clone();
+    contenitore.append(template);
+  }
+
+});
